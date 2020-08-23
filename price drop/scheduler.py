e3d8db66545d4b63946f25198f379e4f121f70af
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2020-08-23 14:23:02 +0530

    Add files via upload

---
+++ price drop/scheduler.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 23 13:41:21 2020
+
+@author: Hasan
+"""
+from app4 import Users
+from scraper import scrape
+from mail import send_mails
+from apscheduler.schedulers.background import BackgroundScheduler
+
+users = Users.query.all()
+def alert_gen():
+    for i in users:
+            try:
+                if scrape(i.url)<i.user_price:
+                    send_mails(i.email,i.username)
+                else:
+                    continue
+            except:
+                continue
+
+sched = BackgroundScheduler(daemon=True)
+  
+sched.add_job(alert_gen,'interval', minutes=3)
+ 
+sched.start()
