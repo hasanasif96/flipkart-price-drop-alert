a4b8289501ab3094a7a54a5a5d39bb927b392868
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2020-08-27 13:09:29 +0530

    Update clock.py

--- price drop/clock.py
+++ price drop/clock.py
@@ -7,10 +7,10 @@ Created on Sun Aug 23 13:41:21 2020
 from app4 import Users
 from scraper import scrape
 from mail import send_mails
-from apscheduler.schedulers.background import BackgroundScheduler
+#from apscheduler.schedulers.background import BackgroundScheduler
 
-users = Users.query.all()
-def alert_gen():
+#users = Users.query.all()
+def alert_gens():
     for i in users:
             try:
                 if scrape(i.url)<i.user_price:
@@ -20,13 +20,30 @@ def alert_gen():
             except:
                 continue
 
-sched = BackgroundScheduler(daemon=False)
+#sched = BackgroundScheduler(daemon=False)
   
-sched.add_job(alert_gen,'interval', minutes=3)
+#sched.add_job(alert_gens,'interval', minutes=3)
  
+#sched.start()
+
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def alert_gen():
+    users = Users.query.all()
+    for i in users:
+            try:
+                if scrape(i.url)<i.user_price:
+                    send_mails(i.email,i.username,i.url)
+                else:
+                    continue
+            except:
+                continue
+
 sched.start()
-
-
 #if __name__ == '__main__':
 #    from apscheduler.schedulers.background import BackgroundScheduler
 #    sched = BackgroundScheduler()
